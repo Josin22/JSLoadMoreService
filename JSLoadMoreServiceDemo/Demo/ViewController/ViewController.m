b6856d5bb3e895ca17937b50bcdae0b5965c8314
乔同新 <1203356793@qq.com>
2017-08-09 17:36:41 +0800

    add GitHub link

--- JSLoadMoreServiceDemo/Demo/ViewController/ViewController.m
+++ JSLoadMoreServiceDemo/Demo/ViewController/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by 乔同新 on 2017/8/7.
 //  Copyright © 2017年 乔同新. All rights reserved.
-//
+//  Github: https://github.com/Josin22/JSLoadMoreService
 
 #import "ViewController.h"
 #import "JSListTableView.h"
